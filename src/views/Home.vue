3e5c5ce3f30f69560ba30d2f53dc92ca0747b59f
pyxis <pyxis@uzuki.live>
2023-05-01 17:41:26 +0900

    add second page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
-   <v-container class="fill-height">
-    <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img height="300" src="@/assets/logo.svg" />
-    </v-responsive>
-  </v-container>
+  <full-page ref="fullpage" :options="options" id="fullpage">
+    <div class="section">
+      <v-container class="fill-height">
+        <v-responsive class="d-flex align-center text-center fill-height">
+            
+        </v-responsive>
+      </v-container>
+    </div>
+    <div class="section">
+      <v-row>
+        <v-col v-for="(item, index) in list" :key="index" cols="12" sm="3">
+          <list-layout :date="item.date" :thumbnail="item.thumbnail" :location="item.location" />
+        </v-col>
+      </v-row>
+    </div>
+  </full-page>
 </template>
 
 <script lang="ts" setup>
+import ListLayout from '@/components/ListLayout.vue';
+import { useAppStore } from '@/store/app';
+import { storeToRefs } from 'pinia';
+
+const store = useAppStore();
+
+const { list } = storeToRefs(store);
+
+const options = {
+  anchors: ['page1', 'page2'],
+  sectionsColor: ['#222222', '#111111']
+}
+
+await store.getListIndex();
 </script>
