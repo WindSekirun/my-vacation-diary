8b944d2a3f16842e0e9e17889aa976fa7aa2e086
WindSekirun <windsekirun@gmail.com>
2023-05-06 10:46:58 +0000

    Deploying to gh-pages from @ WindSekirun/my-vacation-diary@0d0257c82c0d4a565c3989e72f8b77130abca1fc 🚀

--- assets/resizeObserver-5da4e60f.js
+++
@@ -1,1 +0,0 @@
-import{r as f,V as i,E as u,G as R,e as v}from"./index-a99e51c5.js";function d(t){const o=f(),s=f();if(i){const n=new ResizeObserver(e=>{t==null||t(e,n),e.length&&(s.value=e[0].contentRect)});u(()=>{n.disconnect()}),R(o,(e,r)=>{r&&(n.unobserve(r),s.value=void 0),e&&n.observe(e)},{flush:"post"})}return{resizeRef:o,contentRect:v(s)}}export{d as u};
