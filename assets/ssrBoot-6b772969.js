db7e988694d7a6d758b4e5bc64cc02d7991a7761
WindSekirun <windsekirun@gmail.com>
2023-05-06 11:20:12 +0000

    Deploying to gh-pages from @ WindSekirun/my-vacation-diary@9e188f17b1b13fe4f0b52321a83cc755fb9cd394 🚀

---
+++ assets/ssrBoot-6b772969.js
@@ -0,0 +1,1 @@
+import{r as t,o as e,c as s,af as n}from"./index-4031875d.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
