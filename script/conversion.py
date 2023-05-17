5a79b7a7a4be59ea9a139e1fea2f691181e35c44
pyxis <pyxis@uzuki.live>
2023-05-17 14:23:21 +0900

    implement inner gallery, thumbnail

--- script/conversion.py
+++ script/conversion.py
@@ -5,9 +5,9 @@ from pathlib import Path
 import json
 import humanize
 
-date = input('Enter date to convert: ')
+# date = input('Enter date to convert: ')
 
-date = '20230429'
+date = '20230430'
 working_dir = os.path.abspath(f"../contents/{date}")
 path = Path(working_dir)
 thumbnail_path = Path(f'{working_dir}/thumbnail')
@@ -23,18 +23,22 @@ pprint(f'Convert .HEIC to .avif for {len(original_pictures)} items')
 for original in original_pictures:
     target = original.replace(".HEIC", ".avif")
     commands = ["convert", original, target]
-    subprocess.run(commands)
-    compress_size = compress_size + os.stat(target).st_size
-    os.remove(original)
+    # subprocess.run(commands)
+    # compress_size = compress_size + os.stat(target).st_size
+    # os.remove(original)
 
 pprint(f'Convert result: {original_pictures_size} to {humanize.filesize.naturalsize(compress_size)}')
 
 pprint('Generating thumbnails')
 
-thumbnail_path.mkdir(exist_ok=True)
+thumbnail_path.mkdir(parents=True, exist_ok=True)
 thumbnail_command = ["mogrify", '-resize', '10%', '-quality', '60', '-path', str(thumbnail_path),
                      f"{working_dir}/original/*.avif"]
-subprocess.run(thumbnail_command)
+# subprocess.run(thumbnail_command)
+
+if Path(f'{working_dir}/history.kml').exists() is True:
+    commands = ['togeojson', "history.kml", ">", "history.json"]
+    subprocess.run(commands)
 
 compressed_pictures = sorted([str(p) for p in path.glob("./original/*.avif")])
 medias = []
@@ -45,13 +49,9 @@ for compressed in compressed_pictures:
         "thumbnail": f"contents/{date}/thumbnail/{file_name}",
         "type": "image",
         "desc": "",
-        "device": "iPhone 14 Pro Max"
+        
     }
     medias.append(data)
-
-if Path(f'{working_dir}/history.kml').exists() is True:
-    commands = ['togeojson', "history.kml", ">", "history.json"]
-    subprocess.run(commands)
 
 data = {
     "date": date,
@@ -72,8 +72,8 @@ data = {
 json_data = json.dumps(data, indent=4)
 json_file = os.path.abspath(f'../contents/{date}/index.json')
 
-if Path(json_file).exists() is not True:
-    with open(json_file, 'w') as f:
-        f.write(json_data)
+# if Path(json_file).exists() is not True:
+    # with open(json_file, 'w') as f:
+        # f.write(json_data)
 
 pprint('Done')
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,46 +1,82 @@
 <template>
     <div class="mt-5">
         <span class="text-h4 font-weight-bold">Gallery</span>
-        <lightGallery :settings="gallerySettings" class="mt-2">
-            <a v-for="(item, index) in imageMediaList" :data-src="makeUrl(item.original)" :key="index"
-                :data-sub-html="`#caption` + index">
-                <img :alt="`img` + index" :src="makeUrl(item.thumbnail)" />
-            </a>
-            <div v-for="(item, index) in imageMediaList" :key="index" style="display:none" :id="`caption` + index">
-                <h4>{{ item.original }}</h4>
-                <p>
-                    A beautiful Sunrise this morning taken En-route to Keswick not one as
-                    planned but I'm extremely happy I was passing the right place at the
-                    right time....
-                </p>
-            </div>
-        </lightGallery>
+        <div ref="galleryContainer" class="inline-gallery-container mt-5"></div>
+        <v-row class="mt-5 mb-5">
+            <v-col v-for="(item, index) in imageMediaList" :key="index" cols="1">
+                <v-img :src="makeUrl(item.thumbnail)" aspect-ratio="1" cover class="bg-grey-lighten-2"
+                    @click="clickThumbnail(index)" />
+            </v-col>
+        </v-row>
+        <div v-for="(item, index) in imageMediaList" :key="index" style="display:none" :id="`caption` + index">
+            <h4>on </h4>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-import lightGallery from 'lightgallery/vue';
-import { PropType } from 'vue'
+import lightGallery from 'lightgallery';
+import { PropType, ref } from 'vue'
 import { Page, MediaType } from "@/model/page";
 import { makeUrl } from '@/store/api';
 import { computed } from 'vue';
-import { LightGallerySettings } from 'lightgallery/lg-settings';
+import { onMounted } from 'vue';
+import { GalleryItem } from 'lightgallery/lg-utils';
+import { LightGallery } from 'lightgallery/lightgallery';
 
 const props = defineProps({
     page: {
         type: Object as PropType<Page>
     }
 })
-const gallerySettings: LightGallerySettings = {
-    speed: 500,
-    plugins: []
+
+const galleryContainer = ref(null as HTMLElement | null);
+let innerGallery: LightGallery | null = null;
+const imageMediaList = computed(() => props.page?.medias.filter(item => item.type == MediaType.IMAGE));
+
+onMounted(() => {
+    const container = galleryContainer.value as HTMLElement
+    const elements: GalleryItem[] = imageMediaList.value!.map((item, index) => {
+        return {
+            src: makeUrl(item.original),
+            thumb: makeUrl(item.thumbnail),
+            subHtml: `#caption${index}`,
+            alt: `img${index}`
+        }
+    })
+
+    innerGallery = lightGallery(container, {
+        speed: 500,
+        plugins: [],
+        container: container,
+        dynamic: true,
+        hash: false,
+        closable: false,
+        showMaximizeIcon: true,
+        dynamicEl: elements
+    })
+
+    innerGallery.openGallery();
+})
+
+function clickThumbnail(index: number) {
+    const gallery = innerGallery as unknown as LightGallery
+    if (!gallery) {
+        return;
+    }
+    gallery.slide(index);
 }
 
-const imageMediaList = computed(() => props.page?.medias.filter(item => item.type == MediaType.IMAGE));
 
 </script>
 
 <style lang="css">
 @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.7.1/css/lightgallery.css');
 @import url('https://cdn.jsdelivr.net/npm/lightgallery@2.7.1/css/lg-zoom.css');
+
+.inline-gallery-container {
+    width: 100%;
+    height: 0;
+    padding-bottom: 45%;
+}
 </style>
