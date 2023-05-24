9fca974bdaf9cf48ca85fc5688d46a520f60e58e
pyxis <pyxis@uzuki.live>
2023-05-24 22:44:00 +0900

    refactor movements

--- src/components/Movements.vue
+++ src/components/Movements.vue
@@ -2,39 +2,25 @@
     <div v-if="props.movement != null" class="ms-5">
         <ul>
             <li v-if="props.movement.walking && props.movement.walking != 0">
-                <span>
-                    <v-icon icon="mdi-walk" size="x-small"></v-icon> {{ props.movement.walking }} km
-                </span>
+                <v-icon icon="mdi-walk" size="x-small" /> 도보 {{ formatNumber(props.movement.walking) }} km
             </li>
             <li v-if="props.movement.drive && props.movement.drive != 0">
-                <span>
-                    <v-icon icon="mdi-car" size="x-small"></v-icon> {{ props.movement.drive }} km
-                </span>
+                <v-icon icon="mdi-car" size="x-small" /> 운전 {{ formatNumber(props.movement.drive) }} km
             </li>
             <li v-if="props.movement.bus && props.movement.bus != 0">
-                <span>
-                    <v-icon icon="mdi-bus" size="x-small"></v-icon> {{ props.movement.bus }} km
-                </span>
+                <v-icon icon="mdi-bus" size="x-small" /> 버스 {{ formatNumber(props.movement.bus) }} km
             </li>
             <li v-if="props.movement.train && props.movement.train != 0">
-                <span>
-                    <v-icon icon="mdi-train-car-passenger" size="x-small"></v-icon> {{ props.movement.train }} km
-                </span>
+                <v-icon icon="mdi-train-car-passenger" size="x-small" /> 전철 {{ formatNumber(props.movement.train) }} km
             </li>
             <li v-if="props.movement.subway && props.movement.subway != 0">
-                <span>
-                    <v-icon icon="mdi-subway" size="x-small"></v-icon> {{ props.movement.subway }} km
-                </span>
+                <v-icon icon="mdi-subway" size="x-small" /> 지하철 {{ formatNumber(props.movement.subway) }} km
             </li>
             <li v-if="props.movement.airplane && props.movement.airplane != 0">
-                <span>
-                    <v-icon icon="mdi-airplane-takeoff" size="x-small"></v-icon> {{ props.movement.airplane }} km
-                </span>
+                <v-icon icon="mdi-airplane-takeoff" size="x-small" /> 비행기 {{ formatNumber(props.movement.airplane) }} km
             </li>
             <li v-if="props.movement.taxi && props.movement.taxi != 0">
-                <span>
-                    <v-icon icon="mdi-taxi" size="x-small"></v-icon> {{ props.movement.taxi }} km
-                </span>
+                <v-icon icon="mdi-taxi" size="x-small" /> 택시 {{ formatNumber(props.movement.taxi) }} km
             </li>
         </ul>
     </div>
@@ -49,5 +35,9 @@ const props = defineProps({
         type: Object as PropType<Movement>
     }
 })
+
+function formatNumber(number: number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 </script>
